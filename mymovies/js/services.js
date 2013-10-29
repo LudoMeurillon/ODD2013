8d6c5d8302e34d26345f3b9476bdbe203a768ddd
Ludovic Meurillon <ludovic.meurillon@gmail.com>
2013-10-29 23:27:20 +0100

    use omdbapi.com and add tomato rating
    Conflicts:
    	mymovies/src/main/webapp/js/services.js
    	mymovies/src/main/webapp/views/movie-detail.html

--- mymovies/js/services.js
+++ mymovies/js/services.js
@@ -4,9 +4,20 @@
 
 var mymoviesServices = angular.module('mymoviesServices', ['ngResource']);
 
-mymoviesServices.factory('Movie', ['$resource',
-  function($resource){
-    return $resource('data/movies/:movieId.json', {}, {
-      query: {method:'GET', params:{movieId:'movies'}, isArray:true}
-    });
-  }]);
+mymoviesServices
+        .factory('Movie', ['$resource',
+          function($resource){
+            return $resource('data/movies/:movieId.json', {}, {
+              query: {method:'GET', params:{movieId:'movies'}, isArray:true}
+            });
+          }])
+        .factory('movieFromApi', ['$resource',
+          function($resource){
+            return $resource('rest/movie/:movieId', {}, {});
+          }])
+        .factory('omdbApi', ['$resource',
+          function($resource){
+                return $resource('http://www.omdbapi.com/', {}, {
+                        get : {method:"JSONP", params:{tomatoes:true, plot:"full", callback: 'JSON_CALLBACK'}}
+                });
+         }]);
