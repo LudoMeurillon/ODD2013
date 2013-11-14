1eb0fb676381aefe2d978102e0fc8b923bb37eeb
AlexisChemin <alexis.chemin@gmail.com>
2013-11-14 09:37:50 +0100

    Découpage des tests par étape

---
+++ mymovies/src/test/js/step-0.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('My Favorite Movies - Step 0', function() {
+	
+  var mockMymoviesServices;
+
+	// charge le module 'mymoviesControllers'
+  beforeEach(module('mymoviesControllers'));
+  
+  beforeEach(function(){
+	  mockMymoviesServices = {
+	            query: function() {
+	            	return [];
+	            }
+	       };
+  });
+
+  
+  describe('MovieListCtrl', function(){
+    var scope, ctrl;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {     
+      scope =  {};
+      // instancie un controlleur 'MovieListCtrl' avec un scope vide
+      ctrl = $controller('MovieListCtrl', {$scope: scope, 'Movie' : mockMymoviesServices});
+    }));
+
+    
+    it('should be initialized with page title', function() {
+      expect(scope.pageTitle).toBe("My Favorite Movies");
+    });
+
+  });
+  
+  
+});
---
+++ mymovies/src/test/js/step-2.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/* jasmine specs for directives go here */
+
+describe('directives', function() {
+
+  beforeEach(module('mymoviesControllers'));
+
+  describe('vote', function() {
+    it('should print label before stars', function() {
+      inject(function($compile, $rootScope) {
+        var element = $compile('<vote label="test prefix" value="2.5" max="5" stars="10"></vote>')($rootScope);
+        $rootScope.$digest();
+        expect(element.find("ul").text()).toBe("test prefix");
+      });
+    });
+
+    it('should print x stars', function() {
+      inject(function($compile, $rootScope) {
+        var element = $compile('<vote prefix="test" value="2.5" max="5" stars="10"></vote>')($rootScope);
+        $rootScope.$digest();
+        expect(element.find("ul li").length).toBe(10);
+      });
+    });
+
+    it('should print fill full stars regarding to value/max ratio', function() {
+      inject(function($compile, $rootScope) {
+        var element = $compile('<vote prefix="test" value="2.5" max="5" stars="10"></vote>')($rootScope);
+        $rootScope.$digest();
+        expect(element.find("ul li .glyphicon-star").length).toBe(5);
+      });
+    });
+
+    it('should print fill empty stars regarding to value/max ratio', function() {
+      inject(function($compile, $rootScope) {
+        var element = $compile('<vote prefix="test" value="2.5" max="5" stars="10"></vote>')($rootScope);
+        $rootScope.$digest();
+        expect(element.find("ul li .glyphicon-star-empty").length).toBe(5);
+      });
+    });
+  });
+});
